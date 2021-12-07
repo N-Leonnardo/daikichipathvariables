aa559d8cf0eaa3bdc505decd518b2b9fbf1b1633
Leonnardo Nascimento <leonnardoenriquesn@gmail.com>
2021-12-06 16:08:25 -0800

    initial commit

--- src/main/java/com/leonnardo/daikichiproject/DaikichiApplication.java
+++ src/main/java/com/leonnardo/daikichiproject/DaikichiApplication.java
@@ -2,6 +2,7 @@ package com.leonnardo.daikichiproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,13 +20,23 @@ public class DaikichiApplication {
 	public String hello() {
 		return "Welcome!";
 	}
-	@RequestMapping("/today")
-	public String today() {
-		return "Today you will find luck in all your endeavors!";
+	@RequestMapping("/travel/{place}")
+	public String travel(@PathVariable("place") String place)
+	{
+		return "You will soon travel to "+ place;
 	}
-	@RequestMapping("/tomorrow")
-	public String tomorrow() {
-		return "Tomorrow, an opportunity will arise, so be sure to be open to new ideas!";
+	
+	
+	@RequestMapping("/lotto/{number}")
+	public String tomorrow(@PathVariable("number") int number) 
+	{
+		if(number%2 == 0) {
+			return "You will take a grand journey in the near future, but be weary of tempting offers";
+		}else {
+			return "Tomorrow, an opportunity will arise, so be sure to be open to new ideas!";
+			
+		}
+			
 	}
 	
 }
